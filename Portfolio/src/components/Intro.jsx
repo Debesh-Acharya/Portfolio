97f4a71f955e7c9277bfec65c66a9eef002ee484
Debesh-Acharya <debeshacharya30@gmail.com>
2024-06-23 12:51:59 +0530

    css update

--- Portfolio/src/components/Intro.jsx
+++ Portfolio/src/components/Intro.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { TypeAnimation } from 'react-type-animation';
 import EmailIcon from "@mui/icons-material/Email";
 import FadeInSection from "./FadeInSection"; // Ensure this component is correctly implemented
-import { Button } from "@mui/material";
 // import RubiksCubeSolver from "./RubiksCubeSolver";
 
 const Intro = () => {
@@ -14,7 +13,7 @@ const Intro = () => {
         avgTypingDelay={100}
         startDelay={1000}
         cursor={{ show: true, blink: true, element: "|" }}
-        className="text-green-400 text-6xl font-sans"
+        className=" text-teal-200 text-6xl font-sans font-semibold"
         sequence={[
           'hi, ',
           500,
@@ -24,15 +23,15 @@ const Intro = () => {
       />
       {/* Content Section */}
       <FadeInSection>
-        <div className="mt-8 text-4xl text-gray-400 font-light">
+        <div className="mt-8 text-4xl text-gray-400 font-sans font-normal">
           I create stuff sometimes.
         </div>
-        <div className="mt-4 text-lg text-gray-400 max-w-xl pb-12">
+        <div className="mt-4 text-lg text-gray-400 max-w-xl pb-12 font-sans font-light">
           I'm a CSE Student from Bhubaneswar, Odisha, India.
         </div>
         <a
           href="mailto:debeshacharya30@gmail.com"
-          className="flex items-center justify-center text-lg font-bold text-green-400 w-32 h-10 border border-green-400 rounded hover:bg-blue-900 ml-32"
+          className="flex items-center justify-center text-lg font-bold text-teal-200 w-32 h-10 border border-teal-200 rounded hover:bg-gray-700 ml-32"
         >
           <EmailIcon className="mr-2 " />
           {" Say hi!"}
